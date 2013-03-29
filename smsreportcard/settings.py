c2e5c0e4bb7703b383a9782fbf9dcefbece4e313
Kwaw Annor <kwaw@kwaw-HP-625.(none)>
2013-03-29 18:26:52 +0000

    perparing for heroku

--- smsreportcard/settings.py
+++ smsreportcard/settings.py
@@ -20,7 +20,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': 
-        dj_database_url.config(default="sqlite:/smartreport_test.db")
+        dj_database_url.config(default="sqlite:/smartreport.db")
         
 }
 
