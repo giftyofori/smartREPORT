678c3f6a97c3976b8af948405f11e33c9e04de98
Kwaw Annor <kwaw@kwaw-HP-625.(none)>
2013-03-29 18:33:39 +0000

    add smsreportcard

---
+++ smsreportcard/assets/js/smartrepo.js
@@ -0,0 +1,10 @@
+function nextlink(){
+    return document.getElementById('nextlink').innerHTML='<input type="submit" value="next" class="arwnext" />'//'<a href="/report/arw/stage2/">next</a>';
+    //return alert("it works");
+}
+
+function percent(){
+    return alert("cooliiiiiiiiiiii");
+    //document.getElementById('percent').style.background="rgba(0,0,0,0)";    
+
+}
